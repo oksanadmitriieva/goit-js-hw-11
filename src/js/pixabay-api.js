06f007bcdfe3d26440bbd40be506bb7385bacfa3
oksanadmitriieva <145439444+oksanadmitriieva@users.noreply.github.com>
2024-03-25 16:25:39 +0100

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,22 +1,37 @@
-import { inputSearch, showLoader } from '../main';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+const KEY = '42993354-9366f462d179fd9692b03d8e1';
+const BASE_URI = 'https://pixabay.com/api/';
 
-export function fetchPhotoFromPixabay() {
-    const inputValueForForm = inputSearch.value.trim().split(',').join('+');
-    const searchParams = new URLSearchParams({
-        key: "42993354-9366f462d179fd9692b03d8e1",
-        q: [inputValueForForm],
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: true
+export function getImages(query) {
+  const searchSettingObj = {
+    key: KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+  const searchSetting = new URLSearchParams(searchSettingObj);
+  const LINK = `${BASE_URI}?${searchSetting}`;
+  return fetch(LINK)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Search failed');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.total === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+      console.log(data.hits);
+      return data.hits;
+    })
+    .catch(error => {
+      console.log(`${error}`);
     });
-    showLoader();
-    return fetch(`https://pixabay.com/api/?${searchParams}`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        }
-        );
 }
