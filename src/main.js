38df51fb52b8903eaf87261148f870186b49e80c
oksanadmitriieva <145439444+oksanadmitriieva@users.noreply.github.com>
2024-03-25 16:17:29 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,63 +1,36 @@
-import { fetchPhotoFromPixabay } from './js/pixabay-api';
-import { renderPhotos } from './js/render-functions';
-import SimpleLightbox from "simplelightbox";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import { getImages } from './js/pixabay-api.js';
+import { render } from './js/render-functions.js';
 
-const form = document.querySelector('.search-form');
-export const inputSearch = form.elements.search;
-export const listOfPhotos = document.querySelector('.gallery');
-export const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250
-});
-const preloader = document.querySelector('.loader');
-preloader.style.display = 'none';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-export const showLoader = () => {
-    preloader.style.display = 'flex';
-};
-const hideLoader = () => {
-    preloader.style.display = 'none';
-};
+const imagesList = document.querySelector('.images-list');
+const searchForm = document.querySelector('.search-form'); 
+const searchBar = document.querySelector('.search-settings');
+const loading = document.querySelector('.loading-text');
 
 
-form.addEventListener('submit', sendForm);
+searchForm.addEventListener('submit', search);
 
-function sendForm(evt) {
-    evt.preventDefault();
-    listOfPhotos.innerHTML = "";
-    
-    const input = evt.target.elements.search.value.trim();
-    if (input !== '') {
-        window.onload = () => {
-            fetchPhotoFromPixabay()
-                .then((photos) => {
-                    renderPhotos(photos.hits);
-                    hideLoader();
-                })
-                .catch((error) => {
-                    console.log(error);
-                    hideLoader();
-                    iziToast.error({
-                        message: 'Sorry, an error occurred while loading. Please try again!',
-                        theme: 'dark',
-                        progressBarColor: '#FFFFFF',
-                        color: '#EF4040',
-                        position: 'topRight',
-                    });
-                });
-        }
-        window.onload();
-            form.reset();
-        } else {
-            iziToast.show({
-                message: 'Please complete the field!',
-                theme: 'dark',
-                progressBarColor: '#FFFFFF',
-                color: '#EF4040',
-                position: 'topRight',
-            });
-        }
-    }
+function search(event) {
+  event.preventDefault();
+  loading.classList.toggle('visually-hidden');
+  const query = searchBar.value.trim();
+  getImages(query)
+    .then(images => {
+      loading.classList.toggle('visually-hidden');
+      imagesList.innerHTML = '';
+      imagesList.insertAdjacentHTML('beforeend', render(images));
+      const lightbox = new SimpleLightbox('.images-list a', {
+        nav: true,
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 200,
+      });
+      lightbox.refresh();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  searchBar.value = '';
+}
